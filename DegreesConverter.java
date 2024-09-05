343880ca2e2c3c07e83783554760a9762f22d3a1
alfredscyriac <alfredscyriac@gmail.com>
2024-09-05 17:44:46 -0400

    Complete basic iterating with for loop and while loop

--- DegreesConverter.java
+++ DegreesConverter.java
@@ -5,5 +5,17 @@ public class DegreesConverter{
         centigrade = (fahrenheit-32)*5/9;
         // c = 5/9 (f-32)
         System.out.println("Fahrenheit 98.6 converted to Centigrade is "+ centigrade);
+        System.out.println("Iterating using for loop: ");
+        for (float i = 0.0f; i <= 40; i+=5) {
+            centigrade = (i-32)*5/9;
+            System.out.println("Fahrenheit " + i + " converted to Centigrade is "+ centigrade);
+        }
+        System.out.println("Iterating using while loop: ");
+        float k = 0.0f; 
+        while (k <= 40.0f){
+            centigrade = (k-32)*5/9;
+            System.out.println("Fahrenheit " + k + " converted to Centigrade is "+ centigrade);
+            k+=5; 
+        }
     }
 }
