a949f4f8d39c2926c79a5dede9fac9fdff602af1
alfredscyriac <alfredscyriac@gmail.com>
2024-09-11 16:05:48 -0400

    Create method wordIsThere which returns a boolean that checks if string is in array using a for loop, modify so users can enter endless words and program only stop when user enters stop

--- CompareStrings.java
+++ CompareStrings.java
@@ -1,27 +1,30 @@
 import javax.swing.*;
-
-public class Lab4Program1 {
-
-	public static void main(String[] args) {
-	
+public class CompareStrings {
+	public static void main(String[] args) 
+	{
 		String[] wordArray = { "hello", "goodbye", "cat", "dog", "red", "green", "sun", "moon" };
-		String isOrIsNot, inputWord;
-			
-		// This line asks the user for input by popping out a single window
-		// with text input
-		inputWord = JOptionPane.showInputDialog(null, "Enter a word in all lower case:");
-		
-		// if the inputWord is contained within wordArray return true
-		if (wordIsThere(inputWord, wordArray)) 
-			isOrIsNot = "is"; // set to is if the word is on the list
-		else
-			isOrIsNot = "is not"; // set to is not if the word is not on the list
-		
-		// Output to a JOptionPane window whether the word is on the list or not
-		JOptionPane.showMessageDialog(null, "The word " + inputWord + " " + isOrIsNot + " on the list.");
+		String isOrIsNot="is", inputWord="NA";
+		while (!inputWord.equals("STOP")) {
+			inputWord = JOptionPane.showInputDialog(null, "Enter a word in all lower case:");
+			if (inputWord.equals("STOP")) {
+				System.exit(0);
+			}
+			else {
+				if (wordIsThere(inputWord, wordArray)) 
+					isOrIsNot = "is";
+				else
+					isOrIsNot = "is not"; // set to is not if the word is not on the list
+			}
+			// Output to a JOptionPane window whether the word is on the list or not
+			JOptionPane.showMessageDialog(null, "The word " + inputWord + " " + isOrIsNot + " on the list.");
+		}
 	} //main
-
 	public static boolean wordIsThere(String findMe, String[] theList) {
-
-	} // wordIsThere
-} // class Lab4Program1
+		for(int i=0; i<theList.length;i++){
+			if(theList[i].equals(findMe)){
+				return true;
+			}
+		}
+		return false; 
+	}
+}
