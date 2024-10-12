f6a0422a8401b780b4459e3abee859e790fa1ce8
alfredscyriac <alfredscyriac@gmail.com>
2024-10-12 11:47:14 -0400

    Start Project1.java

---
+++ Project1/Project1.java
@@ -0,0 +1,7 @@
+public class Project1 {
+    public static void main(String[] args){
+        Appliance[] refrigerators = new Appliance[100];
+        Appliance[] dishwashers = new Appliance[100];
+        Appliance[] microwaves = new Appliance[100];
+    }
+}
