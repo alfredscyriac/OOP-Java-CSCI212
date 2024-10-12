f8a719e62fe9ff31ab321c39cca5c0f0545a9f46
alfredscyriac <alfredscyriac@gmail.com>
2024-10-12 14:57:48 -0400

    Add GUI components

--- Project1/Project1.java
+++ Project1/Project1.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 public class Project1 {
     public static void main(String[] args){
         Appliance[] refrigerators = new Appliance[100];
@@ -5,7 +6,7 @@ public class Project1 {
         Appliance[] microwaves = new Appliance[100];
         Appliance[] appliances = new Appliance[300]; 
         int applianceCount = 0, fridgeCount = 0, dishwasherCount = 0, microwaveCount = 0;
-        TextFileInput fileInput = new TextFileInput("input.txt");
+        TextFileInput fileInput = new TextFileInput("Project1.txt");
         // Read the file line by line
         String line;
         while ((line = fileInput.readLine()) != null) {
@@ -32,6 +33,33 @@ public class Project1 {
         selectionSort(refrigerators, fridgeCount);
         selectionSort(dishwashers, dishwasherCount);
         selectionSort(microwaves, microwaveCount);
+        JFrame frame = new JFrame("Appliance List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new java.awt.GridLayout(1, 3));
+        // Refrigerators display
+        JTextArea fridgeArea = new JTextArea();
+        for (int i = 0; i < fridgeCount; i++) {
+            fridgeArea.append(refrigerators[i].toString() + "\n");
+        }
+        frame.add(new JScrollPane(fridgeArea));
+
+        // Dishwashers display
+        JTextArea dishwasherArea = new JTextArea();
+        for (int i = 0; i < dishwasherCount; i++) {
+            dishwasherArea.append(dishwashers[i].toString() + "\n");
+        }
+        frame.add(new JScrollPane(dishwasherArea));
+
+        // Microwaves display
+        JTextArea microwaveArea = new JTextArea();
+        for (int i = 0; i < microwaveCount; i++) {
+            microwaveArea.append(microwaves[i].toString() + "\n");
+        }
+        frame.add(new JScrollPane(microwaveArea));
+
+        // Display the frame
+        frame.setSize(600, 400);
+        frame.setVisible(true);
     }
     private static void selectionSort(Appliance[] array, int applianceCount) {
         for (int i = 0; i < applianceCount - 1; i++) {
