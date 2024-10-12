efda136f2404bd3471b3380d26dd2c861e5f05ee
alfredscyriac <alfredscyriac@gmail.com>
2024-10-12 12:31:55 -0400

    Add comments

--- Project1/Project1.java
+++ Project1/Project1.java
@@ -12,11 +12,25 @@ public class Project1 {
             System.out.println("Input file not found. Please ensure Project1.txt is available.");
             return;
         }
+        // This a base case check, if the file has no data then we want to return a message reporting the error
         Scanner scanner = new Scanner(new File("Project1.txt"));
         while (scanner.hasNextLine()) {
             String serialNumber = scanner.nextLine().trim();
             appliances[applianceCount++] = new Appliance(serialNumber);
         }
         scanner.close();
+        // This scanner takes in input from txt file
+        for (int i = 0; i < applianceCount; i++) {
+            Appliance appliance = appliances[i];
+            if (appliance.getApplianceType() == 'R') {
+                refrigerators[fridgeCount++] = appliance;
+            } 
+            else if (appliance.getApplianceType() == 'D') {
+                dishwashers[dishwasherCount++] = appliance;
+            } 
+            else if (appliance.getApplianceType() == 'M') {
+                microwaves[microwaveCount++] = appliance;
+            }
+        }
     }
 }
