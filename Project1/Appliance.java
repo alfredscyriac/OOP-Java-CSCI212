fc78d013191b7aa9fd95574adb57d678948fdbf1
alfredscyriac <alfredscyriac@gmail.com>
2024-10-13 13:04:38 -0400

    Fix comments on Appliance.java

--- Project1/Appliance.java
+++ Project1/Appliance.java
@@ -1,20 +1,34 @@
 public class Appliance{ 
-    private String serialNumber; // Private instance variable for the serial number
-    public Appliance(String serialNumber) { // Constructor that takes a String serial number as a parameter
+    // Private instance variable for the serial number
+    private String serialNumber; 
+    
+    // Constructor that takes a String serial number as a parameter
+    public Appliance(String serialNumber) 
+    { 
         this.serialNumber = serialNumber;
     }
-    public String getSerialNumber() { // Get method 
+    
+    // Get method 
+    public String getSerialNumber() { 
         return serialNumber; 
     }
+
+    // Set method
     public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber; // Set method
+        this.serialNumber = serialNumber; 
     }
+    
+
     public String toString() {
         return serialNumber; 
     }
+
+    // Determines the type of appliance by checking the first char in the string serialNumber
     public char getApplianceType() {
-        return serialNumber.charAt(0); // Determines the type of appliance by checking the first char in the string serialNumber
+        return serialNumber.charAt(0); 
     }
+
+    
     public int compareTo(Appliance other) {
         return this.serialNumber.compareTo(other.serialNumber);
     }
