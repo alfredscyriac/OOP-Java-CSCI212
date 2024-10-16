9886c0dc4a7b1a25468eff572fad399f6abcb5d1
alfredscyriac <alfredscyriac@gmail.com>
2024-10-15 21:37:06 -0400

    Complete Question 1 Exam A

---
+++ Midterm1/Q1A.java
@@ -0,0 +1,14 @@
+public class Q1A {
+    public static void main(String args[]){
+        if(args.length<2){
+            throw new IllegalArgumentException("Not enough arguements");
+        }
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int sum = x + y; 
+        int product = x * y;
+        System.out.println(x+" + "+y+" = "+sum);
+        System.out.println(x+" * "+y+" = "+product);
+
+    }
+}
